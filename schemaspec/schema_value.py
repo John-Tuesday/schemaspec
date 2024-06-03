bc49f69936d0197fa6ee02dba9aa47a1c6620fcc
John-Tuesday <10697715+John-Tuesday@users.noreply.github.com>
2024-06-02 19:42:04 -0500

    Create Choices's base SchemaValue

--- schemaspec/schema_value.py
+++ schemaspec/schema_value.py
@@ -4,13 +4,15 @@
 __all__ = [
     "BaseType",
     "SchemaValue",
+    "ChoiceSchema",
     "BoolSchema",
     "StringSchema",
     "PathSchema",
 ]
 
+import dataclasses
 import pathlib
-from typing import Protocol
+from typing import Protocol, override
 
 type BaseType = str | int | float | bool | list | dict
 """Primative types which can be implicitly converted to and from Python."""
@@ -23,6 +25,10 @@ class SchemaValue[T](Protocol):
         """Text to briefly show what valid input values are."""
         ...
 
+    def is_valid(self, value: T) -> bool:
+        """True if `value` is valid."""
+        return True
+
     def export_value(self, value: T) -> str | None:
         """Export `value` to valid string representation."""
         ...
@@ -32,38 +38,88 @@ class SchemaValue[T](Protocol):
         ...
 
 
-class BoolSchema(SchemaValue[bool]):
+class ChoiceSchema[T](SchemaValue[T]):
+    """A given value is only valid if it is equal to one of given choices.
+
+    If no choices are specified, act just like `SchemaValue`.
+    """
+
+    def __init__(self, default_type_spec: str, choices: tuple[T, ...] = tuple()):
+        self.__choices = choices
+        self.__init_type_spec(default_type_spec)
+
+    @property
+    def choices(self) -> tuple[T, ...]:
+        """Defines the valid choices of a value; empty means no constraint."""
+        return self.__choices
+
+    def __init_type_spec(self, default_spec: str) -> None:
+        self.__type_spec = default_spec
+        if not self.choices:
+            return
+        s = []
+        for choice in self.choices:
+            result = self.export_value(choice)
+            if result is None:
+                raise ValueError("Choice is not a valid type")
+            s.append(result)
+        self.__type_spec = " | ".join(s)
+
+    @override
+    def type_spec(self) -> str:
+        return self.__type_spec
+
+    @override
+    def is_valid(self, value: T) -> bool:
+        return not self.choices or value in self.choices
+
+
+@dataclasses.dataclass
+class BoolSchema(ChoiceSchema[bool]):
     """Bool type schema."""
 
-    def type_spec(self) -> str:
-        return "true | false"
+    def __init__(self, choices: tuple[bool, ...] = ()):
+        super().__init__(default_type_spec="true | false", choices=choices)
 
+    @override
+    def is_valid(self, value: bool) -> bool:
+        return isinstance(value, bool) and super().is_valid(value)
+
+    @override
     def export_value(self, value: bool) -> str | None:
-        if not isinstance(value, bool):
+        if not self.is_valid(value):
             return None
         return "true" if value else "false"
 
+    @override
     def convert_input(self, input: BaseType) -> bool | None:
         """Convert primative to full type."""
-        if not isinstance(input, bool):
+        if not isinstance(input, bool) or not self.is_valid(input):
             return None
         return input
 
 
-class StringSchema(SchemaValue[str]):
+@dataclasses.dataclass
+class StringSchema(ChoiceSchema[str]):
     """String type schema."""
 
-    def type_spec(self) -> str:
-        return '"<string>"'
+    def __init__(self, choices: tuple[str, ...] = ()):
+        super().__init__(default_type_spec='"<string>"', choices=choices)
 
+    @override
+    def is_valid(self, value: str) -> bool:
+        return isinstance(value, str) and super().is_valid(value)
+
+    @override
     def export_value(self, value: str) -> str | None:
-        if not isinstance(value, str):
+        if not self.is_valid(value):
             return None
         return f'"{value}"'
 
+    @override
     def convert_input(self, input: BaseType) -> str | None:
         """Convert primative to full type."""
-        if not isinstance(input, str):
+        if not isinstance(input, str) or not self.is_valid(input):
             return None
         return input
 
@@ -71,16 +127,24 @@ class StringSchema(SchemaValue[str]):
 class PathSchema(SchemaValue[pathlib.Path]):
     """Path type schema."""
 
+    @override
     def type_spec(self) -> str:
         return '"<path>"'
 
+    @override
+    def is_valid(self, value: pathlib.Path) -> bool:
+        return isinstance(value, pathlib.Path) and super().is_valid(value)
+
+    @override
     def export_value(self, value: pathlib.Path) -> str | None:
-        if not isinstance(value, pathlib.Path):
+        if not isinstance(value, pathlib.Path) or not self.is_valid(value):
             return None
         return f'"{value}"'
 
+    @override
     def convert_input(self, input: BaseType) -> pathlib.Path | None:
         """Convert primative to full type."""
         if not isinstance(input, str):
             return None
-        return pathlib.Path(input)
+        path = pathlib.Path(input)
+        return path if self.is_valid(path) else None
