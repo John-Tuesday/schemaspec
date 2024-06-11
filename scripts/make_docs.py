ee9f0a2e458261d099c1a86e8b8530ea3901235f
John-Tuesday <10697715+John-Tuesday@users.noreply.github.com>
2024-06-11 03:39:57 -0500

    Add pdoc script in python.
    This allows more configuration options and more easily work with project
    versons.
    
    I can now enable many more markdown2 extras than before.
    I can now more easily edit docstrings and rendered output instead of
    using jinja.

---
+++ scripts/make_docs.py
@@ -0,0 +1,49 @@
+import os
+import pathlib
+
+import pdoc
+import pdoc.doc
+from pdoc import render, render_helpers, web
+
+from schemaspec.__version__ import __version__
+
+
+def _run_server(*modules: str, host: str = "localhost"):
+    httpd = web.DocServer((host, 8080), [*modules])
+    with httpd:
+        url = f"http://{host}:{httpd.server_port}"
+        web.open_browser(url)
+        try:
+            httpd.serve_forever()
+        except KeyboardInterrupt:
+            httpd.server_close()
+
+
+def _get_doc_output(project_root: pathlib.Path) -> pathlib.Path:
+    version = __version__
+    root = project_root.joinpath(f"docs/api/{version}").resolve()
+    if root.exists():
+        raise ValueError(f"Directory already exists '{root}'")
+    return root
+
+
+def _configure(project_path: pathlib.Path):
+    render_helpers.markdown_extensions["admonitions"] = None
+    render.configure(
+        docformat="restructuredtext",
+        template_directory=project_path.joinpath("templates").resolve(),
+    )
+
+
+def make_docs():
+    project_path = pathlib.Path(os.environ["PDM_PROJECT_ROOT"])
+    _configure(project_path)
+    doc_output = _get_doc_output(project_root=project_path)
+    # # doc = pdoc.doc.Module.from_name("schemaspec.adapters")
+    modules = "schemaspec"
+    pdoc.pdoc(modules, output_directory=doc_output)
+    _run_server(modules)
+
+
+if __name__ == "__main__":
+    make_docs()
